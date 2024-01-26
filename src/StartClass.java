a1ea2d1ad3857cca2a429fbbe4193843b3dc2091
parandszz <samuraei5545@example.com>
2024-01-26 23:46:35 +0330

    add start class

--- src/StartClass.java
+++ src/StartClass.java
@@ -1,7 +1,15 @@
-
+import javax.swing.*;
 
 public class StartClass {
 
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new FootballApp().setVisible(true);
+			}
+		});
 
+	}
 
 }
